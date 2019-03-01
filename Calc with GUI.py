ebf76ae054a488abf3e71888b0dae452e6af7a06
IAmTheRealLinkle <48095904+IAmTheRealLinkle@users.noreply.github.com>
2019-03-01 10:59:40 -0500

    Add files via upload

---
+++ Calc with GUI.py
@@ -0,0 +1,13 @@
+# Program to add, multiply, subtract and divide with a GUI.
+
+#!/usr/bin/python
+# main window 
+import tkinter
+window = tkinter.Tk()
+window.title("Calculator")
+
+# adding text in window
+label = tkinter.Label(window, text = "Hello world!").pack()
+
+# keeping window open
+window.mainloop()
